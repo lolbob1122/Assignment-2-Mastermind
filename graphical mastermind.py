6008b55a669e716d128b74acb89ee891e631f0cb
Luca P. Mattioli <lucapaolo.mattioli@gmail.com>
2024-04-29 13:24:43 +0200

    updated

--- graphical mastermind.py
+++ graphical mastermind.py
@@ -5,8 +5,17 @@ ymax = 1000
 
 resolution = (xmax, ymax)
 screen = pg.display.set_mode(resolution)
+#todo:
+# convert random numbers to colours
+# place the code under a black box at the top of the screen with an option to (show/hide)
+# as the user types the digits, place them in the rows
+# using white and red give oopcorrect and correct
+# add a button to quit
+# one to restart
+# one to change settigns
 
-rect = screen.get_rec()
-
+black = (0, 0, 0)
+scrrect = screen.get_rect()
+pg.draw.rect(screen, black, scrrect)
 
 pg.quit
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while running:
     attempts = 10
     while attempts > 0:
         # Take user input for guess
-        if attempts < 10:
+        if attempts ==   10:
             g = input('Guess a combination of 4 digits, separated by a comma or a space: \n')
         else:
             g = input('Please guess another combination of 4 numbers, separated by a comma or a space: \n')
@@ -27,7 +27,7 @@ while running:
             ### print("\n Guess:", guess, "Length of guess:", len(guess), '\n')  ### Debugging print statement
             # Check if the length of guess is exactly 4
             if len(guess) != 4: 
-                print("/n Please only enter exactly 4 numbers separated by commas or spaces. /n")
+                print("\n Please only enter exactly 4 numbers separated by commas or spaces. \n")
                 continue  # Skip to the next iteration of the loop
 
             #checks for how many are already correct and in the right place
@@ -36,12 +36,10 @@ while running:
             oop_correct = sum(1 for num in guess if num in code) - correct_count
             if correct_count == 4:
                 print("You won!")
-                
                 break
             elif correct_count > 0 or oop_correct > 0:
                 print(correct_count, "digit(s) is(are) correct and in the right place \n", 
                 oop_correct, "other(s) is(are) correct but in the wrong place")
-
             else:
                 print('Incorrect, try again.')
                 
